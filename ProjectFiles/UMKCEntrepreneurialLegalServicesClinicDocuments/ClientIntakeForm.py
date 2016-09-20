ea92c219bc15b689a253d8549b1325432dba3ec0
Bryan Wilson <bgw4g5@mail.umkc.edu>
2016-09-19 20:10:07 -0500

    Create ClientIntakeForm.py

---
+++ ProjectFiles/UMKCEntrepreneurialLegalServicesClinicDocuments/ClientIntakeForm.py
@@ -0,0 +1,26 @@
+#Application for Services
+----
+To be considered for acceptance as a client, you must complete this form and return it to the Entrepreneurial Legal Services Clinic. Acceptance as a client of the UMKC Entrepreneurial Legal Services Clinic is not guaranteed, and is ultimately based upon available of resources and time to provide services, absence of conflicts of interest, financial need of the client, and educational value for our students.
+
+
+print('What is your full name?')
+  clientName=input()
+print('What is the date? (dd/mm/yyyy)')
+  date = input()
+print('What is the name of the entity?')
+  companyName = input()
+print('What is your mailing address')
+  clientAddress = input()
+
+City, State, Zip ______________________________________________________________ Telephone numbers______________cell_________________work_________________home E-mail address_______________________________________________________________
+
+By providing your email address you are giving the Entrepreneurial Legal Services Clinic express permission to contact you via email with matters regarding your business and to contact you regarding other information that may be of interest to you. If you do not want the Entrepreneurial Legal Services Clinic to contact you via email please check the applicable lines below:
+
+______ I do not want the Entrepreneurial Legal Services Clinic to contact me via email regarding my business.
+
+______ I do not want the Entrepreneurial Legal Services Clinic to contact me via email regarding other information that may be of interest to me.
+
+Applicants for services are hereby notified that the University of Missouri-Kansas City and the Entrepreneurial Legal Services Clinic do not discriminate on the basis of race, color, creed, sex, sexual orientation, age, national origin, disability or Vietnam era veterans' status in admission or access to, or treatment or employment in, its programs and activities.
+
+[Client Intake Form](http://www1.law.umkc.edu/clinics/els/application.pdf)
+
