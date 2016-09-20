52d0457956d9f07f71e8a1e1c849ceac7b20da7c
Bryan Wilson <bgw4g5@mail.umkc.edu>
2016-09-20 13:17:35 -0500

    Update ClientIntakeForm.py

--- ProjectFiles/UMKCEntrepreneurialLegalServicesClinicDocuments/ClientIntakeForm.py
+++ ProjectFiles/UMKCEntrepreneurialLegalServicesClinicDocuments/ClientIntakeForm.py
@@ -11,16 +11,40 @@ print('What is the name of the entity?')
   companyName = input()
 print('What is your mailing address')
   clientAddress = input()
+print('What city do you live in?')
+  clientCity = input()
+print('What state do you live in?')
+  clientState = input()
+print('What zip code do you live in?')
+  clientZip = input()
+print('What is your telephone number?')
+  clientPhone
+print('What is your email address? By providing your email address you are giving the Entrepreneurial Legal Services Clinic express permission to contact you via email with matters regarding your business and to contact you regarding other information that may be of interest to you. If you do not want the Entrepreneurial Legal Services Clinic type N/A')
+  clientEmail = input()
 
-City, State, Zip ______________________________________________________________ Telephone numbers______________cell_________________work_________________home E-mail address_______________________________________________________________
+print('Applicants for services are hereby notified that the University of Missouri-Kansas City and the Entrepreneurial Legal Services Clinic do not discriminate on the basis of race, color, creed, sex, sexual orientation, age, national origin, disability or Vietnam era veterans' status in admission or access to, or treatment or employment in, its programs and activities.')
 
-By providing your email address you are giving the Entrepreneurial Legal Services Clinic express permission to contact you via email with matters regarding your business and to contact you regarding other information that may be of interest to you. If you do not want the Entrepreneurial Legal Services Clinic to contact you via email please check the applicable lines below:
+print('Financial Information (required for means testing):')
+print('What is your total expected income for this year?')
+  expectedAnnualIncome = input()
+print('What was your total expected income for last year?')
+  previousAnnualIncome = input()
+print('How much available capital do you have to spend for your entity?')
+  availableCapital = input()
+print('Are you currently employed?')
+  if yes                                #I'm not sure if these next few lines are properly formatted
+    print('If so, where?')
+      employmentLocation = input()
+  if no
+    break
+print('Demographic Information. This information is collected for demographic purposes only; it is anonymous and does not affect your acceptance as a client.')
+print('What is your race?')
+  clientRace = input()
+print('What is your gender?')
+  clientGender = input()
+print(print
 
-______ I do not want the Entrepreneurial Legal Services Clinic to contact me via email regarding my business.
-
-______ I do not want the Entrepreneurial Legal Services Clinic to contact me via email regarding other information that may be of interest to me.
-
-Applicants for services are hereby notified that the University of Missouri-Kansas City and the Entrepreneurial Legal Services Clinic do not discriminate on the basis of race, color, creed, sex, sexual orientation, age, national origin, disability or Vietnam era veterans' status in admission or access to, or treatment or employment in, its programs and activities.
 
 [Client Intake Form](http://www1.law.umkc.edu/clinics/els/application.pdf)
 
+op
